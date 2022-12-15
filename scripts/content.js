94d2aa50d783a2163f788d531e0b538d8bdb86ab
Chris Rolling <chrisrolling@MacBook>
2022-12-15 10:11:00 -0800

    console log

--- scripts/content.js
+++ scripts/content.js
@@ -1,29 +1,30 @@
 
+console.log('highlight-summary initiated');
 
 if (window.getSelection) {
+
+  console.log('window got selected');
 
   const text = window.getSelection().toString();
 
 
 
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
+  // const badge = document.createElement("p");
+  // // Use the same styling as the publish information in an article's header
+  // badge.classList.add("color-secondary-text", "type--caption");
+  // const summary = "hello this is test summary text";
+  // badge.textContent = `Summary: ${summary}`;
 
-  badge.textContent = `Summary: ${gtp3text}`;
-
-
-
-
+  console.log(text);
 
 
-  
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
 
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+
+  // Support for API reference docs and dates
+  //const heading = article.querySelector("h1");
+  //const date = article.querySelector("time")?.parentNode;
+
+  //(date ?? heading).insertAdjacentElement("afterend", badge);
 
 }
 
