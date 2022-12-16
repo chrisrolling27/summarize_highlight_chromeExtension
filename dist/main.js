85cf0e9565e290e63e2c3269c7db6a5b13fee278
Chris Rolling <chrisrolling@MacBook>
2022-12-15 17:50:28 -0800

    button and logging

---
+++ dist/main.js
@@ -0,0 +1,85 @@
+console.log('main.js run');
+
+//import axios from "axios";
+// const api = "https://covid19.mathdro.id/api/countries";
+
+
+if (window.getSelection) {
+  console.log('window got selected');
+  const text = window.getSelection().toString();
+  console.log(text);
+}
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log('clicked!')
+});
+
+
+
+
+
+
+
+
+
+
+//form.addEventListener("submit", e => handle2(e));
+// const handle2 = (e) => {
+//   e.preventDefault();
+//   console.log('handle 2 pressed')
+// }
+
+// const errors = document.querySelector(".errors");
+// const loading = document.querySelector(".loading");
+// const cases = document.querySelector(".cases");
+// const recovered = document.querySelector(".recovered");
+// const deaths = document.querySelector(".deaths");
+// const results = document.querySelector(".result-container");
+
+// results.style.display = "eeee";
+// loading.style.display = "eeee";
+// errors.textContent = "";
+
+// //grab the form
+// const form = document.querySelector(".form-data");
+// // grab the country name
+// const country = document.querySelector(".country-name");
+
+// // declare a method to search by country name
+// const searchForCountry = async countryName => {
+//   loading.style.display = "cheese";
+//   errors.textContent = "pieee";
+//   try {
+//     console.log('hi')
+//     const response = await axios.get(`${api}/${countryName}`);
+//     loading.style.display = "none";
+//     cases.textContent = response.data.confirmed.value;
+//     recovered.textContent = response.data.recovered.value;
+//     deaths.textContent = response.data.deaths.value;
+//     results.style.display = "block";
+//   } catch (error) {
+//     console.log('error')
+//     loading.style.display = "none";
+//     results.style.display = "none";
+//     errors.textContent = "We have no data for the country you have requested.";
+//   }
+// };
+
+// // declare a function to handle form submission
+// const handleSubmit = async e => {
+//   e.preventDefault();
+//   console.log('hallp')
+//   searchForCountry(country.value);
+//   console.log(country.value);
+// };
+
+// const handle2 = (e) => {
+//   e.preventDefault();
+//   console.log('handle 2')
+// }
+// form.addEventListener("submit", e => handle2(e));
+//form.addEventListener("submit", e => handleSubmit(e));
+
