1b40acf35ab8871e5b3b55fb2e7daae8a8987186
chris <chrisrolling27@gmail.com>
2023-03-23 14:56:21 -0700

    sample

--- background.js
+++ background.js
@@ -1,10 +1,28 @@
 
 console.log('background.js run')
 
-  chrome.runtime.onMessage.addListener(function(request, sender) {
-    if (request.action === "highlight") {
-        console.log('highlighted text got!')
-        const highlightedText = window.getSelection().toString();
-        chrome.tabs.sendMessage(sender.tab.id, {action: "summarize", text: highlightedText});
-    }
-  });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+      chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          files: ["./foreground.js"]
+      })
+          .then(() => {
+              console.log("INJECTED THE FOREGROUND SCRIPT.");
+          })
+          .catch(err => console.log(err));
+  }
+});
+
+
+
+
+
+  // chrome.runtime.onMessage.addListener(function(request, sender) {
+  //   if (request.action === "highlight") {
+  //       console.log('highlighted text got!')
+  //       const highlightedText = window.getSelection().toString();
+  //       chrome.tabs.sendMessage(sender.tab.id, {action: "summarize", text: highlightedText});
+  //   }
+  // });
