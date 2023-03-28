cca64b00ec5335cf8abd8fd1cdfccfdcd3841067
chris <chrisrolling27@gmail.com>
2023-03-27 20:34:30 -0700

    slimmed

--- background.js
+++ background.js
@@ -1,12 +1,5 @@
 console.log('background.js run')
 
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.local.set({
-//       name: "Emma"
-//   });
-// });
-
-
 function alertHighlight() {
   let text = window.getSelection().toString();
   console.log(text);
@@ -17,8 +10,6 @@ function alertHighlight() {
 
 }
 
-
-
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
@@ -26,50 +17,3 @@ chrome.action.onClicked.addListener((tab) => {
     });
 });
 
-
-//To get our foreground script to actually embed into the users tab(s) we need to first monitor 
-//their browsing experience with the tabs API. We do this monitoring in the backround script:
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
-//       chrome.scripting.executeScript({
-//           target: { tabId: tabId },
-//           files: ["./foreground.js"]
-//       })
-//           .then(() => {
-//               console.log("INJECTED THE FOREGROUND SCRIPT.");
-//           })
-//           .catch(err => console.log(err));
-//   }
-// });
-
-
-
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.message === 'get_name') {
-//       chrome.storage.local.get('name', data => {
-//           if (chrome.runtime.lastError) {
-//               sendResponse({
-//                   message: 'fail'
-//               });
-
-//               return;
-//           }
-
-//           sendResponse({
-//               message: 'success',
-//               payload: data.name
-//           });
-//       });
-
-//       return true;
-//   }})
-
-
-
-  // chrome.runtime.onMessage.addListener(function(request, sender) {
-  //   if (request.action === "highlight") {
-  //       console.log('highlighted text got!')
-  //       const highlightedText = window.getSelection().toString();
-  //       chrome.tabs.sendMessage(sender.tab.id, {action: "summarize", text: highlightedText});
-  //   }
-  // });
