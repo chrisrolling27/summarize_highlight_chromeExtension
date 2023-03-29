98cc842de1a0ca0e577cba9174d429fa42fc36db
chris <chrisrolling27@gmail.com>
2023-03-28 20:50:27 -0700

    works

--- background.js
+++ background.js
@@ -1,46 +1,48 @@
 console.log('background.js run');
-
 async function alertHighlight() {
   let text = window.getSelection().toString();
   console.log(text);
 
   if (text != '') {
     try {
-      const summary = await fetchGPTSummary(text);
-      alert(summary);
-    } catch(error) {
+      const response = await fetch('https://api.openai.com/v1/engines/text-davinci-002/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer sk-XX`,
+        },
+        body: JSON.stringify({
+          prompt: `Please summarize the following text: ${text}`,
+          max_tokens: 50,
+          n: 1,
+          stop: null,
+          temperature: 0.7,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data.choices && data.choices.length > 0) {
+        console.log(data);
+        const summary = data.choices[0].text.trim();
+        console.log(summary);
+        alert(summary);
+      } else {
+        console.error("Unexpected API response format:", data);
+        alert("Failed to fetch summary. Please try again.");
+      }
+
+    } catch (error) {
       alert(error);
     }
-    } 
-    if (text == '') {
-      alert('ya need highlight');
-    }
+  } else {
+    alert('you need to highlight text');
   }
+}
 
-
-  async function fetchGPTSummary(text) {
-    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer sk-`,
-      },
-      body: JSON.stringify({
-        prompt: `Please summarize the following text: ${text}`,
-        max_tokens: 50,
-        n: 1,
-        stop: null,
-        temperature: 0.7,
-      }),
-    });
-
-    const data = await response.json();
-    return data.choices[0].text.trim();
-  }
-
-
-  chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: alertHighlight
-    })})
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: alertHighlight
+  });
+});
